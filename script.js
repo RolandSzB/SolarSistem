1fc697e14491d0098366ce3198a0d912d1491c83
SziBe Roland <roland.szilagyi-benedek@emanuel.ro>
2023-04-03 11:37:26 +0300

    sun made

--- script.js
+++ script.js
@@ -7,9 +7,17 @@ function draw() {
 }
 function solarSistem() {
   //fields
+  fill("ffffff");
   ellipse(400, 300, 650, 200);
   ellipse(400, 300, 550, 190);
   ellipse(400, 300, 450, 180);
   ellipse(400, 300, 350, 175);
   ellipse(400, 300, 250, 170);
+  //sun
+  fill("#fcc603");
+  circle(400, 300, 75);
+  triangle(395, 260, 405, 260, 400, 240);
+  triangle(440, 295, 465, 300, 440, 305);
+  triangle(395, 340, 405, 340, 400, 360);
+  triangle(360, 295, 360, 305, 335, 300);
 }
