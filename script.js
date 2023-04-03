1dcfc33b6776bb60df4b2fd03a27e256c7c50994
SziBe Roland <roland.szilagyi-benedek@emanuel.ro>
2023-04-03 11:06:26 +0300

    fields made

---
+++ script.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(800, 800);
+}
+function draw() {
+  background("#ffffff");
+  solarSistem();
+}
+function solarSistem() {
+  //fields
+  ellipse(400, 300, 650, 200);
+  ellipse(400, 300, 550, 190);
+  ellipse(400, 300, 450, 180);
+  ellipse(400, 300, 350, 175);
+  ellipse(400, 300, 250, 170);
+}
